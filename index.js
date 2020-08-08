0403cbb315372f618a371d4cbc410c168ea9a244
iwanbazz <iwan.bazz@gmail.com>
2020-08-09 04:46:29 +0800

    Modify index

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
-const router = require('./router')
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const router = require('./router');
 
-const REQUEST_LIMIT = '10mb'
-const PORT = process.env.PORT || 8888
+const REQUEST_LIMIT = '10mb';
+const PORT = process.env.PORT || 8888;
 
 const start = async () => {
   http.listen(PORT, () =>
-    console.info(`Server started http://localhost:${PORT}`)
-  )
+    console.info(`Server started http://https://ekspress.herokuapp.com:${PORT}`)
+  );
 
   app
     .disable('etag')
     .disable('x-powered-by')
-    .use(cors({
-      origin: true,
-      credentials: true
-    }))
+    .use(
+      cors({
+        origin: true,
+        credentials: true,
+      })
+    )
     .options('*', cors())
     .use(
       bodyParser.urlencoded({
         limit: REQUEST_LIMIT,
-        extended: false
+        extended: false,
       })
     )
     .use(bodyParser.json({ limit: REQUEST_LIMIT }))
@@ -32,11 +34,11 @@ const start = async () => {
       fileUpload({
         limits: { fileSize: 25 * 1024 * 1024 }, // 25 mb
         useTempFiles: false,
-        abortOnLimit: true
+        abortOnLimit: true,
       })
     )
     .use(router())
-    .get(['/', '/health'], (_, __) => __.send('OK'))
-}
+    .get(['/', '/health'], (_, __) => __.send('OK'));
+};
 
-start()
+start();
